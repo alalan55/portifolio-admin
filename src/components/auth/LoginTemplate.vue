9eaf29c8d31e215822a3bfafc16e7338ffad06a1
aoliveira <aoliveira@keener.io>
2023-01-19 16:24:09 -0300

    Ininia dark mode

--- src/components/auth/LoginTemplate.vue
+++ src/components/auth/LoginTemplate.vue
@@ -1,12 +1,19 @@
 <script setup>
-// const teste = ()=>{
-//     document.body.classList.toggle('dark')
-// }
+import { useDark, useToggle } from "@vueuse/core";
+const isDark = useDark({
+  selector: "body",
+  attribute: "class",
+  valueDark: "dark",
+  valueLight: "light",
+});
+const toggleDard = useToggle(isDark);
 </script>
 
 <template>
   <div>
     <h1>Autenticação</h1>
+    <h2>{{ isDark }}</h2>
+    <button @click="toggleDard()">teste</button>
   </div>
 </template>
 
