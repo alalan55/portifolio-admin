2e465334813bfd908879fcf1ed80ffc3d8c6dc74
aoliveira <aoliveira@keener.io>
2023-01-19 15:53:28 -0300

    Startup

---
+++ src/components/auth/LoginTemplate.vue
@@ -0,0 +1,13 @@
+<script setup>
+// const teste = ()=>{
+//     document.body.classList.toggle('dark')
+// }
+</script>
+
+<template>
+  <div>
+    <h1>Autenticação</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import LoginTemplate from "../components/auth/LoginTemplate.vue";
+</script>
+<template>
+  <main>
+    <LoginTemplate />
+  </main>
+</template>
