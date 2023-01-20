83930b7b266436f1c22b59d6d58a62550194be0c
alalan55 <alla.ao4975@gmail.com>
2023-01-19 22:24:22 -0300

    Criação de guarda de rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import { useAuthStore } from "../stores/auth";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,14 +9,17 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: "/auth",
@@ -23,6 +27,15 @@ const router = createRouter({
       component: () => import("../views/LoginView.vue"),
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const store = useAuthStore();
+
+  if (to.meta.requiresAuth && !store.$token) next({ name: "auth" });
+  else if (to.name == "auth" && store.$token) next({ name: "home" });
+  else next();
+  
 });
 
 export default router;
